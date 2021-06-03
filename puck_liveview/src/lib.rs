e685694cdae46c5ff8e0e1a958502cc1e4bd7d19
teymour-aldridge <teymour.aldridge@icloud.com>
2021-06-03 11:27:29 +0100

    Rework the `chat` example to use `malvolio`.

--- puck_liveview/src/lib.rs
+++ puck_liveview/src/lib.rs
@@ -5,3 +5,8 @@ pub mod client;
 pub mod dom;
 pub mod html;
 pub mod init;
+
+pub mod prelude {
+    pub use crate::html::{IntoWrappedBodyNode, WrappedBodyNode};
+    pub use malvolio::prelude::*;
+}
