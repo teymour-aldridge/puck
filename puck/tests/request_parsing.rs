7b2423aa14a93c5e4eb7617ce1fcdcc0ce794d23
teymour-aldridge <teymour.aldridge@icloud.com>
2021-06-04 21:37:40 +0100

    fix (maybe)

--- puck/tests/request_parsing.rs
+++ puck/tests/request_parsing.rs
@@ -1,53 +1,56 @@
-use itertools::Itertools;
-use proptest::prelude::*;
-use puck::{
-    request::{Body, Method},
-    Request,
-};
+#[cfg(feature = "_test_fuzzing")]
+mod test {
+    use itertools::Itertools;
+    use proptest::prelude::*;
+    use puck::{
+        request::{Body, Method},
+        Request,
+    };
 
-fn ascii_headers() -> impl Strategy<Value = (Vec<(String, String)>, usize)> {
-    // 19 because we add one!
-    prop::collection::vec(("[a-zA-Z][a-zA-Z-]+", "[a-zA-Z][a-zA-Z-]+"), 1..19).prop_flat_map(
-        |vec| {
-            let len = vec.len();
-            (Just(vec), 0..len)
-        },
-    )
-}
-
-proptest! {
-    #[test]
-    #[cfg(feature="_test_fuzzing")]
-    fn pt_request_parsing(headers in ascii_headers(), body: String) {
-        test_http_request_parsing(headers, body)
+    fn ascii_headers() -> impl Strategy<Value = (Vec<(String, String)>, usize)> {
+        // 19 because we add one!
+        prop::collection::vec(("[a-zA-Z][a-zA-Z-]+", "[a-zA-Z][a-zA-Z-]+"), 1..19).prop_flat_map(
+            |vec| {
+                let len = vec.len();
+                (Just(vec), 0..len)
+            },
+        )
     }
-}
 
-fn test_http_request_parsing(headers: (Vec<(String, String)>, usize), body: String) {
-    let mut headers = headers.0;
-    headers.push(("Host".to_string(), "example.com".to_string()));
-    let mut req = Request::build("http://example.com/")
-        .headers(headers.clone())
-        .method(Method::Get)
-        .body(Body::from_string(body.clone()))
-        .build();
+    proptest! {
+        #[test]
+        #[cfg(feature="_test_fuzzing")]
+        fn pt_request_parsing(headers in ascii_headers(), body: String) {
+            test_http_request_parsing(headers, body)
+        }
+    }
 
-    let mut buffer = vec![];
-    req.write(&mut buffer)
-        .expect("failed to write request into buffer");
-    let cursor = std::io::Cursor::new(buffer);
-    let req = Request::parse(cursor)
-        .expect("failed to parse")
-        .expect("the request body was empty");
-    let parsed_body_string = req.body.into_string().expect("failed to parse body");
-    assert_eq!(parsed_body_string, body);
-    for ((input_key, input_value), (parsed_key, parsed_value)) in req
-        .headers
-        .into_iter()
-        .sorted()
-        .zip(headers.into_iter().sorted())
-    {
-        assert_eq!(input_key, parsed_key);
-        assert_eq!(input_value, parsed_value);
+    fn test_http_request_parsing(headers: (Vec<(String, String)>, usize), body: String) {
+        let mut headers = headers.0;
+        headers.push(("Host".to_string(), "example.com".to_string()));
+        let mut req = Request::build("http://example.com/")
+            .headers(headers.clone())
+            .method(Method::Get)
+            .body(Body::from_string(body.clone()))
+            .build();
+
+        let mut buffer = vec![];
+        req.write(&mut buffer)
+            .expect("failed to write request into buffer");
+        let cursor = std::io::Cursor::new(buffer);
+        let req = Request::parse(cursor)
+            .expect("failed to parse")
+            .expect("the request body was empty");
+        let parsed_body_string = req.body.into_string().expect("failed to parse body");
+        assert_eq!(parsed_body_string, body);
+        for ((input_key, input_value), (parsed_key, parsed_value)) in req
+            .headers
+            .into_iter()
+            .sorted()
+            .zip(headers.into_iter().sorted())
+        {
+            assert_eq!(input_key, parsed_key);
+            assert_eq!(input_value, parsed_value);
+        }
     }
 }
--- puck/tests/response_parsing.rs
+++ puck/tests/response_parsing.rs
@@ -1,68 +1,71 @@
-use std::io::Cursor;
+#[cfg(feature = "_test_fuzzing")]
+mod test {
+    use std::io::Cursor;
 
-use itertools::Itertools;
-use proptest::prelude::*;
-use puck::{encoder::Encoder, request::Body, Response};
+    use itertools::Itertools;
+    use proptest::prelude::*;
+    use puck::{encoder::Encoder, request::Body, Response};
 
-fn ascii_headers() -> impl Strategy<Value = (Vec<(String, String)>, usize)> {
-    // 19 because we add one!
-    prop::collection::vec(("[a-zA-Z][a-zA-Z-]+", "[a-zA-Z][a-zA-Z-]+"), 1..19).prop_flat_map(
-        |vec| {
-            let len = vec.len();
-            (Just(vec), 0..len)
-        },
-    )
-}
-
-proptest! {
-    #[test]
-    #[cfg(feature="_test_fuzzing")]
-    fn pt_test_response_parsing(headers in ascii_headers(), body: String) {
-        test_http_response_parsing(headers, body);
+    fn ascii_headers() -> impl Strategy<Value = (Vec<(String, String)>, usize)> {
+        // 19 because we add one!
+        prop::collection::vec(("[a-zA-Z][a-zA-Z-]+", "[a-zA-Z][a-zA-Z-]+"), 1..19).prop_flat_map(
+            |vec| {
+                let len = vec.len();
+                (Just(vec), 0..len)
+            },
+        )
     }
-}
 
-fn test_http_response_parsing(headers: (Vec<(String, String)>, usize), body: String) {
-    let mut headers = headers.0;
+    proptest! {
+        #[test]
+        #[cfg(feature="_test_fuzzing")]
+        fn pt_test_response_parsing(headers in ascii_headers(), body: String) {
+            test_http_response_parsing(headers, body);
+        }
+    }
 
-    let response = Response::build()
-        .headers(headers.clone())
-        .body(Body::from_string(body.clone()))
-        .build();
+    fn test_http_response_parsing(headers: (Vec<(String, String)>, usize), body: String) {
+        let mut headers = headers.0;
 
-    let mut encoder = Encoder::new(response);
+        let response = Response::build()
+            .headers(headers.clone())
+            .body(Body::from_string(body.clone()))
+            .build();
 
-    let mut buf = vec![];
-    encoder
-        .write_tcp_stream(&mut buf)
-        .expect("failed to encode response");
+        let mut encoder = Encoder::new(response);
 
-    let cursor = Cursor::new(buf);
+        let mut buf = vec![];
+        encoder
+            .write_tcp_stream(&mut buf)
+            .expect("failed to encode response");
 
-    let mut response = Response::parse(cursor)
-        .expect("failed to parse response")
-        .expect("blank response");
+        let cursor = Cursor::new(buf);
 
-    let response_body = response
-        .take_body()
-        .into_string()
-        .expect("failed to interpret body as string");
+        let mut response = Response::parse(cursor)
+            .expect("failed to parse response")
+            .expect("blank response");
 
-    assert_eq!(response_body, body);
+        let response_body = response
+            .take_body()
+            .into_string()
+            .expect("failed to interpret body as string");
 
-    headers.push((
-        "Content-Type".to_string(),
-        "application/octet-stream".to_string(),
-    ));
+        assert_eq!(response_body, body);
 
-    for ((input_key, input_value), (parsed_key, parsed_value)) in response
-        .headers()
-        .clone()
-        .into_iter()
-        .sorted()
-        .zip(headers.into_iter().sorted())
-    {
-        assert_eq!(input_key, parsed_key);
-        assert_eq!(input_value, parsed_value);
+        headers.push((
+            "Content-Type".to_string(),
+            "application/octet-stream".to_string(),
+        ));
+
+        for ((input_key, input_value), (parsed_key, parsed_value)) in response
+            .headers()
+            .clone()
+            .into_iter()
+            .sorted()
+            .zip(headers.into_iter().sorted())
+        {
+            assert_eq!(input_key, parsed_key);
+            assert_eq!(input_value, parsed_value);
+        }
     }
 }
