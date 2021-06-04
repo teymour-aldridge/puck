f13550f9850195d35b97a198ee16ede1738f0070
teymour-aldridge <teymour.aldridge@icloud.com>
2021-06-04 21:21:12 +0100

    Set exit code

--- scripts/proptest.sh
+++ scripts/proptest.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
+
+$exit_code=0
+
 end=$((SECONDS+7200))
 
 while [ $SECONDS -lt $end ]; do
     (cd puck && cargo test pt_ --release --features _test_fuzzing "$@")
     if [[ x$? != x0 ]] ; then
-        exit $?
+        exit_code=1
+        break
     fi
 done
+
+exit $exit_code
