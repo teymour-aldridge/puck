74a0827566189fa1f175dd3df1ed5070692a3771
teymour-aldridge <teymour.aldridge@icloud.com>
2021-06-04 20:56:34 +0100

    specify target correctly

--- scripts/proptest.sh
+++ scripts/proptest.sh
@@ -2,7 +2,7 @@
 end=$((SECONDS+7200))
 
 while [ $SECONDS -lt $end ]; do
-    (cd puck && cargo test pt_ --features _test_fuzzing)
+    (cd puck && cargo test pt_ --release --features _test_fuzzing "$@")
     if [[ x$? != x0 ]] ; then
         exit $?
     fi
